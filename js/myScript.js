ac02361b87ccfee7282648a88d13aa915dfdf037
GEOMG <geomgee@naver.com>
2024-01-08 23:20:39 +0900

    Docs : login modal 추가

--- js/myScript.js
+++ js/myScript.js
@@ -143,4 +143,86 @@ $(function () {
         $('body').css('overflow', 'auto');
     });
 
+
+
+    /*--------- product --------*/
+    let cardSection = $(".cardSection-item-list");
+    let car = $(".car");
+    let health = $(".health");
+    let travel = $(".travel");
+    let driver = $(".driver");
+    let etc = $(".etc");
+    let ProductNavList1 = $(".show1");
+    let ProductNavList2 = $(".show2");
+    let ProductNavList3 = $(".show3");
+    let ProductNavList4 = $(".show4");
+    let ProductNavList5 = $(".show5");
+    let ProductNavList = $(".productNav ul li");
+    let ProductNavListA = $(".productNav ul li a");
+    let navMenu = $(".nav-menu ul li");
+    let i = $(this).index();
+
+    cardSection.show();
+    ProductNavList.eq(i).toggleClass("gg-text-blue");
+
+    ProductNavList.eq(0).click(function () {
+        cardSection.show();
+    })
+
+    navMenu.click(function () {
+        location.reload(true);
+    })
+
+    ProductNavList1.click(function () {
+        car.show();
+        health.hide();
+        travel.hide();
+        driver.hide();
+        etc.hide();
+    })
+
+    ProductNavList2.click(function () {
+        car.hide();
+        health.show();
+        travel.hide();
+        driver.hide();
+        etc.hide();
+    })
+
+    ProductNavList3.click(function () {
+        car.hide();
+        health.hide();
+        travel.show();
+        driver.hide();
+        etc.hide();
+    })
+
+    ProductNavList4.click(function () {
+        car.hide();
+        health.hide();
+        travel.hide();
+        driver.show();
+        etc.hide();
+    })
+
+    ProductNavList5.click(function () {
+        car.hide();
+        health.hide();
+        travel.hide();
+        driver.hide();
+        etc.show();
+    })
+
+    ProductNavList.click(function () {
+        this.addClass("color", "red");
+        alert("ProductNavListA");
+    })
+
+    /*modal*/
+    cardSection.click(function () {
+        $('#myModal').modal('show')
+    })
+    /*--------- event page --------*/
+    /*--------- counsel --------*/
+
 });
